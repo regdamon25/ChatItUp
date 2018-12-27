64c2df170414698282ab62b33953669ad916d1ac
Reggie Burrus <reggie.burrus@outlook.com>
2018-12-27 09:31:19 -0600

    Third Commit

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express()
 const server = require('http').Server(app);
-const io = module.exports.io = require('socket.io')(app);
+const io = module.exports.io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3231;
 
@@ -10,6 +10,6 @@ const SocketManager = require('./SocketManager');
 app.use( express.static(__dirname + '/../../build') );
 io.on('connection', SocketManager);
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log("Connected to port:" + PORT);
 });
